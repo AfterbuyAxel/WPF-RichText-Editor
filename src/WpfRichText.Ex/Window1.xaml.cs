1782fc405323c1922a483de21146007d01ef7cdd
Axel Scheiwe <ascheiwe@afterbuy.de>
2013-04-08 16:06:09 +0200

    added src files

---
+++ src/WpfRichText.Ex/Window1.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace WpfRichText
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            this.DataContext = new PageViewModel();
+        }
+    }
+
+    #region PageViewModel
+    public class PageViewModel : ObservableBase
+    {  
+        public DelegateCommand GetXamlCommand { get; private set; }
+
+        #region Constructor
+        public PageViewModel()
+        {
+            GetXamlCommand = new DelegateCommand(() =>
+            {
+                MessageBox.Show(this.Text);
+            });
+        }
+        #endregion
+        
+        #region Name
+        private string text = string.Empty;
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+                this.RaisePropertyChanged(p => p.Text);
+            }
+        }
+        #endregion
+    }
+    #endregion
+
+    #region Observable
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public static class ObservableBaseEx
+    {
+        public static void RaisePropertyChanged<T, TProperty>(this T observableBase, Expression<Func<T, TProperty>> expression) where T : ObservableBase
+        {
+            observableBase.RaisePropertyChanged(observableBase.GetPropertyName(expression));
+        }
+
+        public static string GetPropertyName<T, TProperty>(this T owner, Expression<Func<T, TProperty>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                var unaryExpression = expression.Body as UnaryExpression;
+                if (unaryExpression != null)
+                {
+                    memberExpression = unaryExpression.Operand as MemberExpression;
+
+                    if (memberExpression == null)
+                        throw new NotImplementedException();
+                }
+                else
+                    throw new NotImplementedException();
+            }
+
+            var propertyName = memberExpression.Member.Name;
+            return propertyName;
+        }
+    }
+    #endregion
+}
---
+++ src/WpfRichText/Controls/RichTextEditor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRichText
+{
+    /// <summary>
+    /// Interaction logic for BindableRichTextbox.xaml
+    /// </summary>
+    public partial class RichTextEditor : UserControl
+    {
+		/// <summary></summary>
+        public static readonly DependencyProperty TextProperty =
+          DependencyProperty.Register("Text", typeof(string), typeof(RichTextEditor),
+          new PropertyMetadata(string.Empty));
+
+		/// <summary></summary>
+		public RichTextEditor()
+        {
+            InitializeComponent();
+        }
+
+		/// <summary></summary>
+		public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { 
+                SetValue(TextProperty, value);
+            }
+        }
+    }
+}
